48df2b188dff6528a406f7943841cb776f0c55bf
unknown <Chamon562@gmail.com>
2022-06-16 16:45:54 -0400

    Add different banner image above search

--- client/src/components/HeroBanner.jsx
+++ client/src/components/HeroBanner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Stack, Typography, Button } from "@mui/material";
-import HeroBannerImg from "../assets/images/banner.png";
+import HeroBannerImg from "../assets/images/bannerImage.jpeg";
 const HeroBanner = () => {
   return (
     <Box
@@ -41,7 +41,15 @@ const HeroBanner = () => {
       >
         Exercise
       </Typography>
-      <img src={HeroBannerImg} alt="banner" className="hero-banner-img" />
+      <img
+        src={HeroBannerImg}
+        alt="banner"
+        className="hero-banner-img"
+        style={{
+          boxShadow:
+            " rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+        }}
+      />
     </Box>
   );
 };
--- client/src/components/SearchExercises.jsx
+++ client/src/components/SearchExercises.jsx
@@ -4,9 +4,49 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 const SearchExercises = () => {
   return (
     <Stack alignItems="center" mt="38px" justifyContent="center" p="20px">
-      <Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "48px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
         Great Exercises You <br /> Should Know
       </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
+            },
+            width: { lg: "800px", sm: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "4px",
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#5DBAE8",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "16px", xs: "12px" },
+            height: '55px',
+            position: 'absolute',
+            right: 0
+          }}
+          
+        >
+          Search
+        </Button>
+      </Box>
     </Stack>
   );
 };
